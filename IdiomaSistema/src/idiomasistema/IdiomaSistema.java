ea1a4620e4fe1ac16a026cd89ab08d63e7203d07
rodolphomoura <134257269+rodolphomoura@users.noreply.github.com>
2023-05-22 03:05:25 -0300

    Add files via upload

---
+++ IdiomaSistema/src/idiomasistema/IdiomaSistema.java
@@ -0,0 +1,12 @@
+
+package idiomasistema;
+import java.util.Locale;
+public class IdiomaSistema {
+
+    public static void main(String[] args) {
+        Locale Idioma = Locale.getDefault();
+        System.out.println("seu idioma é:");
+        System.out.println(Idioma);
+    }
+    
+}
